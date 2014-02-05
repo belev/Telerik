687ee6a0107b94075c8b01200c3286fce54d77f3
Martin Belev <martinbelev93@gmail.com>
2014-02-05 01:08:42 -0800

    .init

--- OOP/03.ExtensionMethodsLambdaExprLINQ/09.StudentProject/Tester.cs
+++ OOP/03.ExtensionMethodsLambdaExprLINQ/09.StudentProject/Tester.cs
@@ -10,6 +10,9 @@ namespace _09.StudentProject
     //Create a List<Student> with sample students. Select only the students that are from group number 2. Use LINQ query. Order the students by FirstName.
     class Tester
     {
+        /// <summary>
+        /// 09.Select only the students that are from group number 2. Use LINQ query. Order the students by FirstName.
+        /// </summary>
         static void FindStudentsFromSecondGroupWithLinq()
         {
             var studentsFromSecondGroupWithLinq =
@@ -25,6 +28,105 @@ namespace _09.StudentProject
             }
         }
 
+        /// <summary>
+        /// 10.Implement the previous using the same query expressed with extension methods.
+        /// </summary>
+        static void FindStudentsFromSecondGroupWithLambda()
+        {
+            var studentsFromSecondGroupWithLambda = students.Where(x => x.GroupNumber == 2).OrderBy(x => x.FirstName);
+
+            foreach (var student in studentsFromSecondGroupWithLambda)
+            {
+                Console.WriteLine(student);
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// 11.Extract all students that have email in abv.bg. Use string methods and LINQ.
+        /// </summary>
+        static void FindStudentsWithEmailInAbv()
+        {
+            var studentWithEmailInAbv =
+                from student in students
+                where student.Email.EndsWith("abv.bg")
+                select student;
+
+            foreach (var student in studentWithEmailInAbv)
+            {
+                Console.WriteLine(student);
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// 12.Extract all students with phones in Sofia. Use LINQ.
+        /// </summary>
+        static void FindStudentsWithSofiaPhoneNumber()
+        {
+            var studentsWithSofiaPhoneNumbers =
+                from student in students
+                where student.Tel.StartsWith("02")
+                select student;
+
+            foreach (var student in studentsWithSofiaPhoneNumbers)
+            {
+                Console.WriteLine(student);
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// 13.Select all students that have at least one mark Excellent (6) into a new anonymous class that has properties – FullName and Marks. Use LINQ.
+        /// </summary>
+        static void FindStudentsWithAtLeastOneExcellentMark()
+        {
+            var studentsWithExcellentMark =
+                from student in students
+                where student.ContainMark(6)
+                select new { FullName = student.FirstName + " " + student.LastName, Marks = student.GetMarks() };
+
+            foreach (var student in studentsWithExcellentMark)
+            {
+                Console.WriteLine("{0} -> {1}", student.FullName, student.Marks);
+            }
+        }
+
+        /// <summary>
+        /// 14.Write down a similar program that extracts the students with exactly  two marks "2". Use extension methods.
+        /// </summary>
+        static void FindStudentsWithExactlyTwoMarks()
+        {
+            var studentsWithExactlyTwoMarks =
+                from student in students
+                where student.MarksCount == 2
+                select new { FullName = student.FirstName + " " + student.LastName, Marks = student.GetMarks() };
+
+            foreach (var student in studentsWithExactlyTwoMarks)
+            {
+                Console.WriteLine("{0} -> {1}", student.FullName, student.Marks);
+            }
+
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// 15.Extract all Marks of the students that enrolled in 2006. (The students from 2006 have 06 as their 5-th and 6-th digit in the FN).
+        /// substring from element on position 4 (not position 5) because the indeces start from 0
+        /// </summary>
+        static void FindStudentMarksEnrolledIn2006()
+        {
+            var studentsEnrolledIn2006 =
+                from student in students
+                where student.FN.Substring(4, 2) == "06"
+                select new { FullName = student.FirstName + " " + student.LastName, FN = student.FN, Marks = student.GetMarks() };
+
+            foreach (var student in studentsEnrolledIn2006)
+            {
+                Console.WriteLine("{0} - FN: {1} -> {2}", student.FullName, student.FN, student.Marks);
+            }
+        }
+
         static List<Student> students;
         static void Main()
         {
@@ -37,88 +139,20 @@ namespace _09.StudentProject
                 new Student("Penka", "Stoicheva", "318206", "0899111111", "penka@abv.bg", new List<int>{2, 2, 3, 3}, 5)
 
             };
+
             FindStudentsFromSecondGroupWithLinq();
-            ////09.Select only the students that are from group number 2. Use LINQ query. Order the students by FirstName.
-            //var studentsFromSecondGroupWithLinq =
-            //    from student in students
-            //    where student.GroupNumber == 2
-            //    orderby student.FirstName
-            //    select student;
 
-            //foreach (var student in studentsFromSecondGroupWithLinq)
-            //{
-            //    Console.WriteLine(student);
-            //    Console.WriteLine();
-            //}
+            FindStudentsFromSecondGroupWithLambda();
 
-            ////10.Implement the previous using the same query expressed with extension methods.
-            //var studentsFromSecondGroupWithLambda = students.Where(x => x.GroupNumber == 2).OrderBy(x => x.FirstName);
+            FindStudentsWithEmailInAbv();
 
-            //foreach (var student in studentsFromSecondGroupWithLambda)
-            //{
-            //    Console.WriteLine(student);
-            //    Console.WriteLine();
-            //}
+            FindStudentsWithSofiaPhoneNumber();
 
-            ////11.Extract all students that have email in abv.bg. Use string methods and LINQ.
+            FindStudentsWithAtLeastOneExcellentMark();
 
-            //var studentWithEmailInAbv =
-            //    from student in students
-            //    where student.Email.EndsWith("abv.bg")
-            //    select student;
+            FindStudentsWithExactlyTwoMarks();
 
-            //foreach (var student in studentWithEmailInAbv)
-            //{
-            //    Console.WriteLine(student);
-            //    Console.WriteLine();
-            //}
-
-            ////12.Extract all students with phones in Sofia. Use LINQ.
-            //var studentsWithSofiaPhoneNumbers =
-            //    from student in students
-            //    where student.Tel.StartsWith("02")
-            //    select student;
-
-            //foreach (var student in studentsWithSofiaPhoneNumbers)
-            //{
-            //    Console.WriteLine(student);
-            //    Console.WriteLine();
-            //}
-
-            ////13.Select all students that have at least one mark Excellent (6) into a new anonymous class that has properties – FullName and Marks. Use LINQ.
-            //var studentsWithExcellentMark =
-            //    from student in students
-            //    where student.ContainMark(6)
-            //    select new { FullName = student.FirstName + " " + student.LastName, Marks = student.GetMarks() };
-
-            //foreach (var student in studentsWithExcellentMark)
-            //{
-            //    Console.WriteLine("{0} -> {1}", student.FullName, student.Marks);
-            //}
-
-            ////14.Write down a similar program that extracts the students with exactly  two marks "2". Use extension methods.
-            //var studentsWithExactlyTwoMarks =
-            //    from student in students
-            //    where student.MarksCount == 2
-            //    select new { FullName = student.FirstName + " " + student.LastName, Marks = student.GetMarks() };
-
-            //foreach (var student in studentsWithExactlyTwoMarks)
-            //{
-            //    Console.WriteLine("{0} -> {1}", student.FullName, student.Marks);
-            //}
-            //Console.WriteLine();
-
-            ////15.Extract all Marks of the students that enrolled in 2006. (The students from 2006 have 06 as their 5-th and 6-th digit in the FN).
-            ////substring from element on position 4 (not position 5) because the indeces start from 0
-            //var studentsEnrolledIn2006 =
-            //    from student in students
-            //    where student.FN.Substring(4, 2) == "06"
-            //    select new { FullName = student.FirstName + " " + student.LastName, FN = student.FN, Marks = student.GetMarks() };
-
-            //foreach (var student in studentsEnrolledIn2006)
-            //{
-            //    Console.WriteLine("{0} - FN: {1} -> {2}", student.FullName, student.FN, student.Marks);
-            //}
+            FindStudentMarksEnrolledIn2006();
         }
     }
 }
