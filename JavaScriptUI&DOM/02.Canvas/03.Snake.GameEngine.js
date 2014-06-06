709e2b1e778ddb0388ff3177e6d46620aa728f37
Martin Belev <martinbelev93@gmail.com>
2014-06-06 12:13:33 -0700

    Refactoring some of the variables from snake

--- JavaScriptUI&DOM/02.Canvas/03.Snake.GameEngine.js
+++ JavaScriptUI&DOM/02.Canvas/03.Snake.GameEngine.js
@@ -1,4 +1,18 @@
 function GameEngine() {
+	CELL_SIZE = 10,
+    FIELD_WIDTH = 600,
+    FIELD_HEIGHT = 450;
+	
+    var SNAKE_INITIAL_LENGTH = 4,
+    LEFT_ARROW_KEY_NUMBER = 37,
+    UP_ARROW_KEY_NUMBER = 38,
+    RIGHT_ARROW_KEY_NUMBER = 39,
+    DOWN_ARROW_KEY_NUMBER = 40,
+    UP_DIRECTION = 'up',
+    DOWN_DIRECTION = 'down',
+    LEFT_DIRECTION = 'left',
+    RIGHT_DIRECTION = 'right';
+
     var collisionDetector,
         snake,
         food,
--- JavaScriptUI&DOM/02.Canvas/03.Snake.js
+++ JavaScriptUI&DOM/02.Canvas/03.Snake.js
@@ -1,17 +1,4 @@
 function snake() {
-    CELL_SIZE = 10;
-    FIELD_WIDTH = 600;
-    FIELD_HEIGHT = 450;
-    SNAKE_INITIAL_LENGTH = 4;
-    LEFT_ARROW_KEY_NUMBER = 37;
-    UP_ARROW_KEY_NUMBER = 38;
-    RIGHT_ARROW_KEY_NUMBER = 39;
-    DOWN_ARROW_KEY_NUMBER = 40;
-    UP_DIRECTION = 'up';
-    DOWN_DIRECTION = 'down';
-    LEFT_DIRECTION = 'left';
-    RIGHT_DIRECTION = 'right';
-
     var canvas = document.getElementById('canvas-field');
     ctx = canvas.getContext('2d');
 
