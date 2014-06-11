f259da1089ee98270725e93169b21ca7fceacf7d
Martin Belev <martinbelev93@gmail.com>
2014-06-10 20:16:41 -0700

    Kinetic js presentation and homework added.

---
+++ JavaScriptUI&DOM/03.KineticJS/drawer.js
@@ -0,0 +1,165 @@
+﻿function drawer(containerID) {
+    var STROKE_COLOR = '#77B300',
+        TEXT_COLOR = 'black',
+        RECT_HEIGHT = 50,
+        RECT_WIDTH = 145,
+        FONT_SIZE = 18,
+        ARROW_SIZE = 5,
+        BEIZER_STROKE_WIDTH = 2;
+
+    var layer = new Kinetic.Layer();
+
+    function getRectWithText(x, y, cornerRadius, text) {
+        var currRect = new Kinetic.Rect({
+            x: x,
+            y: y,
+            width: RECT_WIDTH,
+            height: RECT_HEIGHT,
+            strokeWidth: 3,
+            stroke: STROKE_COLOR,
+            cornerRadius: cornerRadius
+        });
+
+        var currText = new Kinetic.Text({
+            x: x,
+            y: y + (RECT_HEIGHT / 3),
+            text: text,
+            fontSize: FONT_SIZE,
+            fontFamily: 'Calibri',
+            width: RECT_WIDTH,
+            align: 'center',
+            fill: TEXT_COLOR
+        });
+
+        layer.add(currRect);
+        layer.add(currText);
+    }
+
+    function makeFemaleRect(x, y, name) {
+        getRectWithText(x, y, 25, name);
+    }
+
+    function makeMaleRect(x, y, name) {
+        getRectWithText(x, y, 10, name);
+    }
+
+    function makeLinkBetweenMaleFemale(leftX, leftY) {
+        var currLine = new Kinetic.Shape({
+            drawFunc: function (context) {
+                context.beginPath();
+                context.moveTo(leftX + RECT_WIDTH, leftY + RECT_HEIGHT / 2);
+                context.lineTo(leftX + RECT_WIDTH * 1.5, leftY + RECT_HEIGHT / 2);
+                context.stroke(this);
+            },
+            strokeWidth: BEIZER_STROKE_WIDTH,
+            stroke: STROKE_COLOR
+        });
+        layer.add(currLine);
+    }
+
+    function makeConnection(leftParentX, leftParentY, childX, childY) {
+        var startX = leftParentX + RECT_WIDTH * 1.25;
+        var startY = leftParentY + RECT_HEIGHT / 2;
+        var endX = childX + RECT_WIDTH / 2;
+        var endY = childY;
+        innerGetBezierLine(startX, startY, endX, endY, layer);
+
+        function innerGetBezierLine(stX, stY, eX, eY) {
+            var beizerCPdx = Math.abs(eX - stX) / 10;
+            var beizerCPdY = Math.abs(eY - stY) * 0.95;
+            var currLine = new Kinetic.Shape({
+                drawFunc: function (context) {
+                    context.beginPath();
+                    context.moveTo(stX, stY);
+                    context.bezierCurveTo(stX + beizerCPdx, stY + beizerCPdY, eX - beizerCPdx, eY - beizerCPdY, eX, eY);
+                    context.stroke(this);
+                    context.beginPath();
+                    context.lineTo(eX + ARROW_SIZE, eY - ARROW_SIZE);
+                    context.lineTo(eX - ARROW_SIZE, eY - ARROW_SIZE);
+                    context.lineTo(eX, eY);
+                    context.fill(this);
+                },
+                strokeWidth: BEIZER_STROKE_WIDTH,
+                fill: STROKE_COLOR,
+                stroke: STROKE_COLOR
+            });
+
+            layer.add(currLine);
+        }
+    }
+
+    function renderTree(members) {
+        var stageSize = (function () {
+            var maxWidth = 0;
+            var maxDepth = 0;
+            for (var i = 0; i < members.length; i++) {
+                if (maxWidth < members[i].depthX) {
+                    maxWidth = members[i].depthX;
+                }
+                if (maxDepth < members[i].depthY) {
+                    maxDepth = members[i].depthY;
+                }
+            }
+            return {
+                maxXIndexes: maxWidth + 1,
+                maxDepth: maxDepth + 1
+            };
+        }());
+
+        var stage = new Kinetic.Stage({
+            container: containerID,
+            width: stageSize.maxXIndexes * RECT_WIDTH * 1.5,
+            height: stageSize.maxDepth * RECT_HEIGHT * 2
+        });
+
+        var shownMembers = [];
+
+        for (var i = 0; i < members.length; i++) {
+            if (shownMembers.indexOf(members[i].name) < 0) {
+                var coords = getCoords(members[i]);
+
+                if (members[i].female) {
+                    makeFemaleRect(coords.X, coords.Y, members[i].name);
+
+                    if (members[i].partner) {
+                        var pCoords = getCoords(members[i].partner);
+
+                        var connectionX = Math.min(coords.X, pCoords.X);
+                        var connectionY = Math.min(coords.Y, pCoords.Y);
+
+                        makeLinkBetweenMaleFemale(connectionX, connectionY);
+                    }
+                }
+                else {
+                    makeMaleRect(coords.X, coords.Y, members[i].name);
+                }
+
+                // if there is a parent - show the link
+                if (members[i].parent) {
+                    var coords1 = getCoords(members[i].parent);
+                    var coords2 = getCoords(members[i].parent.partner);
+
+                    var minX = Math.min(coords1.X, coords2.X);
+                    var minY = Math.min(coords1.Y, coords2.Y);
+
+                    makeConnection(minX, minY, coords.X, coords.Y);
+                }
+
+                shownMembers.push(members[i].name);
+            }
+        }
+
+        function getCoords(member) {
+            return {
+                X: member.depthX * RECT_WIDTH * 1.5,
+                Y: member.depthY * RECT_HEIGHT * 2
+            };
+        }
+
+        stage.add(layer);
+    }
+
+    return {
+        renderTree: renderTree
+    };
+}
---
+++ JavaScriptUI&DOM/03.KineticJS/treeBuilder.js
@@ -0,0 +1,120 @@
+﻿function getMembersAsTree(familyMembers) {
+
+    function TreeNode(name) {
+        this.parent = false;
+        this.name = name;
+        this.partner = false;
+        this.children = [];
+        this.female = false;
+    }
+
+    function createTreeFromMembers() {
+        var allFamilyMembers = [];
+
+        for (var i = 0; i < familyMembers.length; i++) {
+            var currentMember = getCurrentMemberAsTreeNode(familyMembers[i].mother);
+            currentMember.female = true;
+
+            // make connection between husbands and wives
+            if (!currentMember.partner) {
+                currentMember.partner = getCurrentMemberAsTreeNode(familyMembers[i].father);
+                currentMember.partner.partner = currentMember;
+            }
+
+            // for all the children - creates (or gets) a child and sets its parent property
+            for (var j = 0; j < familyMembers[i].children.length; j++) {
+                var currentChild = getCurrentMemberAsTreeNode(familyMembers[i].children[j]);
+                currentChild.parent = currentMember;
+
+                currentMember.children.push(currentChild);
+            }
+        }
+
+        return allFamilyMembers;
+
+        function getCurrentMemberAsTreeNode(name) {
+            for (var i = 0; i < allFamilyMembers.length; i++) {
+
+                if (name == allFamilyMembers[i].name) {
+                    return allFamilyMembers[i];
+                }
+            }
+
+            var memberAsNode = new TreeNode(name);
+            allFamilyMembers.push(memberAsNode);
+
+            return memberAsNode;
+        }
+    }
+
+    function findGrandParents() {
+        var grandParents = [];
+
+        for (var i = 0; i < membersAsTree.length; i++) {
+            if (!membersAsTree[i].parent && membersAsTree[i].children.length > 0) {
+                grandParents.push(membersAsTree[i]);
+            }
+        }
+
+        return grandParents;
+    }
+
+    function setNodeDepths (grandparents, members) {
+        var depthY = 0;
+        var depthsX = [];
+
+        while (grandParents.length > 0) {
+
+            if (!depthsX[depthY]) {
+                depthsX[depthY] = 0;
+            }
+
+            var len = grandParents.length;
+            var shownPartners = [];
+
+            for (var i = 0; i < len; i++) {
+                // we have added this member already
+                if (shownPartners.indexOf(grandParents[i].name) != -1) {
+                    continue;
+                }
+
+                grandparents[i].depthX = depthsX[depthY]++;
+                grandparents[i].depthY = depthY;
+
+                if (grandparents[i].partner) {
+                    shownPartners.push(grandparents[i].partner.name);
+                    grandparents[i].partner.depthX = depthsX[depthY]++;
+                    grandparents[i].partner.depthY = depthY;
+                }
+                else {
+                    // without partner can not have children
+                    continue;
+                }
+
+                // get parents children, if current parent children are not set, than get the children from its partner
+                var children = grandparents[i].children;
+
+                if (children.length === 0) {
+                    children = grandparents[i].partner.children;
+                }
+
+                for (var j = 0; j < children.length; j++) {
+                    grandparents.push(children[j]);
+                }
+            }
+
+            // remove all grand parents which are used
+            grandparents.splice(0, len);
+            depthY++;
+        }
+
+        return members;
+    }
+
+    var membersAsTree = createTreeFromMembers();
+    var grandParents = findGrandParents();
+
+    var membersWithDepths = setNodeDepths(grandParents, membersAsTree);
+
+    return membersWithDepths;
+}
