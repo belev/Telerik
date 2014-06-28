64d531e52166f4b886676be932e406f6931d893a
Martin Belev <martinbelev93@gmail.com>
2014-06-28 07:15:24 -0700

    AMD and RequireJS homework added.

---
+++ JavaScriptOOP/05.AMDAndRequireJS/app-config.js
@@ -0,0 +1,32 @@
+﻿/// <reference path="libs/require.js" />
+/// <reference path="libs/mustache.js" />
+/// <reference path="app/controls.js" />
+/// <reference path="app/data.js" />
+require.config({
+    paths: {
+        jquery: "libs/jquery-2.0.3",
+        mustache: "libs/mustache",
+        data: "app/data",
+        controls: "app/controls"
+    }
+});
+
+require(["jquery", "mustache", "data", "controls"], function ($, mustache, data, controls) {
+    var personTemplateString = $("#student-template").html();
+    var template = mustache.compile(personTemplateString);
+    var comboBoxView = controls.ComboBox(data.getPeople());
+
+    var ComboBoxHtml = comboBoxView.render(template);
+
+    $("#content").html(ComboBoxHtml);
+
+    $("#content").find('.person-list').on("click", "li", function () {
+        $(this).siblings().hide();
+        $(this).addClass("selected");
+    });
+
+    $("#content").find('.person-list').on("click", "li.selected", function () {
+        $("li.selected").removeClass("selected");
+        $(this).parent().children().show();
+    });
+});
