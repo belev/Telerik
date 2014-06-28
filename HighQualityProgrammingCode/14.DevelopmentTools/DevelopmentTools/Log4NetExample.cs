3e97c32f9c60e9451106d6cacf44b3e0b4735f09
Martin Belev <martinbelev93@gmail.com>
2014-06-28 08:29:10 -0700

    Development tools homework and presentation added.

---
+++ HighQualityProgrammingCode/14.DevelopmentTools/DevelopmentTools/Log4NetExample.cs
@@ -0,0 +1,35 @@
+﻿namespace T4Template
+{
+    using System;
+
+    using log4net;
+    using log4net.Config;
+
+    public class Log4NetExample
+    {
+        private static readonly ILog exception = LogManager.GetLogger("Exception");
+
+        private static readonly ILog debug = LogManager.GetLogger("Debug");
+
+        static void Main(string[] args)
+        {
+            BasicConfigurator.Configure();
+            debug.Info("Debug logger says hello.");
+
+            try
+            {
+                debug.Warn("Please enter valid int number!");
+
+                var number = int.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                exception.Error(ex.Message);
+            }
+            finally
+            {
+                debug.Info("Hello from debug logger again. All previous steps have finished.");
+            }
+        }
+    }
+}
---
+++ HighQualityProgrammingCode/14.DevelopmentTools/DevelopmentTools/SimpleTemplate.cs
@@ -0,0 +1,7 @@
+﻿
+public class SimpleTemplate
+{
+	// create at 06/28/2014 08:00:26
+	// Hello from T4 template.
+}
+
